d7e140c724b9cc36f09c6b874cbcfeaef0a494d6
mondrags <mondrags@uci.edu>
2018-02-22 13:08:02 -0800

    added main page

---
+++ js/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/Looping_Clouds.mp4",
+    poster: "img/welcome_background.png"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
